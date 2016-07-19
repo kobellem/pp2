26cc3768a37a1932294a0dc545d8253593102933
kobellem <kobellem@vub.ac.be>
2016-07-19 15:43:52 +0200

    can send serialized track over tcp

--- src/infra/infra.rkt
+++ src/infra/infra.rkt
@@ -1,12 +1,15 @@
 #lang racket
 ;Author Koen Bellemans
 
-(require "server.rkt" "requestHandler.rkt")
+(require "load-test-track.rkt" "server.rkt" "requestHandler.rkt")
+(require "data/node.rkt")
 
 (define (infra)
-  ;Create TCP server
+  ;load the testrack
+  (define track (load-test-track))
+  ;create TCP server
   (define server (new server% [port 3000]))
-  (define requestHandler (new requestHandler%))
+  (define requestHandler (new requestHandler% [track track]))
   (send server listen (lambda (in out)
     (send requestHandler handle-request in out))))
 
--- src/infra/requestHandler.rkt
+++ src/infra/requestHandler.rkt
@@ -1,6 +1,7 @@
 #lang racket
 ;Author Koen Bellemans
 
+(require racket/serialize)
 (provide requestHandler%)
 
 (define requestHandler%
@@ -8,6 +9,7 @@
     (super-new)
     (public handle-request)
     ;variable initialization
+    (init-field track)
     (init)
     ;public methods
     (define (handle-request in out)
@@ -15,10 +17,14 @@
         (println (string-append "Request: " req))
         (cond
           [(string=? req "test")(test out)]
+          [(string=? req "get-track")(get-track out)]
           [else (unknown req out)])))
     ;private methods
     (define (test out)
       (write "ok" out)
+      (flush-output out))
+    (define (get-track out)
+      (write (serialize track) out)
       (flush-output out))
     (define (unknown req out)
       (write (string-append "Unknown request: " req) out)
