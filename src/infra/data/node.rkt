aa52ac9a57b9a108f7b05421b7b36bafd9042254
kobellem <kobellem@vub.ac.be>
2016-07-19 15:42:49 +0200

    made track and node serializable

--- src/infra/data/node.rkt
+++ src/infra/data/node.rkt
@@ -1,34 +1,49 @@
 #lang racket
 ;Author Koen Bellemans
 
-(provide node%)
+(provide make-node)
 
-(define node%
-  (class object%
-    (super-new)
-    ;public methods
-    (public get-id get-coordinates add-segment get-segments is-switch? switch)
-    ;variable initialization
-    (init-field id)
-    (init x y)
-    (define coordinates (cons x y))
-    (define seg1 #f)
-    (define seg2 #f)
-    (define seg3 #f)
-    ;public methods
-    (define (get-id) id)
-    (define (get-coordinates) coordinates)
-    (define (add-segment sid)
-      (cond
-        [(not seg1) (set! seg1 sid)]
-        [(not seg2) (set! seg2 sid)]
-        [(not seg3) (set! seg3 sid)]
-        [else (error "Node" id "already has 3 segments")]))
-    (define (get-segments) (cons seg1 seg2))
-    (define (is-switch?) (if seg3 #t #f))
-    (define (switch) (if (is-switch?) (swap-seg) #f))
-    ;private methods
-    (define (swap-seg)
-      (let ([temp seg2])
-        (set! seg2 seg3)
-        (set! seg3 temp)))))
+(define (make-node id x y)
+  (define node (new node%))
+  (send node set-id! id)
+  (send node set-coordinates! x y)
+  node)
+
+(define-serializable-class* node% object% (externalizable<%>)
+  (super-new)
+  ;public methods
+  (public set-id! get-id set-coordinates! get-coordinates add-segment get-segments is-switch? switch)
+  ;variable initialization
+  ;No initialization arguments to support serialization
+  (define id #f)
+  (define coordinates #f)
+  (define seg1 #f)
+  (define seg2 #f)
+  (define seg3 #f)
+  ;public methods
+  (define (set-id! id_) (set! id id_))
+  (define (get-id) id)
+  (define (set-coordinates! x y) (set! coordinates (cons x y)))
+  (define (get-coordinates) coordinates)
+  (define (add-segment sid)
+    (cond
+      [(not seg1) (set! seg1 sid)]
+      [(not seg2) (set! seg2 sid)]
+      [(not seg3) (set! seg3 sid)]
+      [else (error "Node" id "already has 3 segments")]))
+  (define (get-segments) (cons seg1 seg2))
+  (define (is-switch?) (if seg3 #t #f))
+  (define (switch) (if (is-switch?) (swap-seg) #f))
+  ;private methods
+  (define (swap-seg)
+    (let ([temp seg2])
+      (set! seg2 seg3)
+      (set! seg3 temp)))
+  ;externalizable interface
+  (define/public (externalize) (list id coordinates seg1 seg2 seg3))
+  (define/public (internalize lst)
+    (set-id! (car lst))
+    (set-coordinates! (caadr lst) (cdadr lst))
+    (set! seg1 (caddr lst))
+    (set! seg2 (cadddr lst))
+    (set! seg3 (car (cddddr lst)))))
--- src/infra/data/track.rkt
+++ src/infra/data/track.rkt
@@ -4,40 +4,42 @@
 (require racket/set)
 (provide track%)
 
-(define track%
-  (class object%
-    (super-new)
-    ;public methods
-    (public add-segment get-segment get-length)
-    ;variable initialization
-    (init)
-    (define segments (mutable-set))
-    ;public methods
-    (define (add-segment id node1 node2)
-      (define segment (cons id (cons node1 node2)))
-      (set-add! segments segment)
-      (send node1 add-segment id)
-      (send node2 add-segment id))
-    (define (get-segment id)
-      (find-segment segments id))
-    (define (get-length id)
-      (let ([seg (find-segment segments id)])
-        (if seg
-          (calculate-length (cdr seg))
-          #f)))
-    ;private methods
-    (define (find-segment st id)
-      (if (not (set-empty? st))
-        (let ([current (set-first st)])
-          (if (eq? (car current) id)
-            current
-            (begin
-              (set-remove! st current) 
-              (find-segment st id))))
-        #f))))
-    (define (calculate-length nodes)
-      (let* ([node1 (car nodes)]
-             [node2 (cdr nodes)]
-             [c1 (send node1 get-coordinates)]
-             [c2 (send node2 get-coordinates)])
-        (sqrt (+ (expt (- (car c2) (car c1)) 2) (expt (- (cdr c2) (cdr c1)) 2)))))
+(define-serializable-class* track% object% (externalizable<%>)
+  (super-new)
+  ;public methods
+  (public add-segment get-segment get-length)
+  ;variable initialization
+  (init)
+  (define segments (mutable-set))
+  ;public methods
+  (define (add-segment id node1 node2)
+    (define segment (cons id (cons node1 node2)))
+    (set-add! segments segment)
+    (send node1 add-segment id)
+    (send node2 add-segment id))
+  (define (get-segment id)
+    (find-segment segments id))
+  (define (get-length id)
+    (let ([seg (find-segment segments id)])
+      (if seg
+        (calculate-length (cdr seg))
+        #f)))
+  ;private methods
+  (define (find-segment st id)
+    (if (not (set-empty? st))
+      (let ([current (set-first st)])
+        (if (eq? (car current) id)
+          current
+          (begin
+            (set-remove! st current) 
+            (find-segment st id))))
+      #f))
+  (define (calculate-length nodes)
+    (let* ([node1 (car nodes)]
+           [node2 (cdr nodes)]
+           [c1 (send node1 get-coordinates)]
+           [c2 (send node2 get-coordinates)])
+      (sqrt (+ (expt (- (car c2) (car c1)) 2) (expt (- (cdr c2) (cdr c1)) 2)))))
+  ;externalizable interface
+  (define/public (externalize) segments)
+  (define/public (internalize s) (set! segments s)))
