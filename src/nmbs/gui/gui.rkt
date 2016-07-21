c352b9d738df59cd4854aa1e7fb97e1b672cca97
kobellem <kobellem@vub.ac.be>
2016-07-21 19:41:51 +0200

    added color and drawing nodes

--- src/nmbs/gui/gui.rkt
+++ src/nmbs/gui/gui.rkt
@@ -11,16 +11,19 @@
     ;variable initialization
     (init-field track)
     (define canvas #f)
+    (define green (make-color 0 255 0 1))
     ;publice methods
     (define (refresh)
-      (print "refresh"))
+      (draw-track))
     ;private methods
     (define (make-window x y)
       (define window (new frame% [label "pp2"][width x][height y]))
       (set! canvas (new canvas% [parent window]))
       (send window show #t))
     (define (draw-track)
-      (let ([dc (send canvas get-dc)])
+      (let* ([dc (send canvas get-dc)]
+             [pen (new pen% [color green])])
+        (send dc set-pen pen)
         (send track for-each-segment (lambda (seg)
           (let* ([nodes (send seg get-nodes)]
                  [node1 (car nodes)]
@@ -29,6 +32,7 @@
                  [y1 (send node1 get-y)]
                  [x2 (send node2 get-x)]
                  [y2 (send node2 get-y)])
+            (send dc draw-ellipse (- x1 5) (- y1 5) 10 10)
             (send dc draw-line x1 y1 x2 y2))))))
     ;initialization
     (make-window 1024 512)
