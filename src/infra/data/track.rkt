7e3553f1d684f930669224727479e61e71138ec0
kobellem <kobellem@vub.ac.be>
2016-07-21 15:13:30 +0200

    can retrieve a lis of segment ids

--- src/infra/data/track.rkt
+++ src/infra/data/track.rkt
@@ -7,32 +7,36 @@
 (define-serializable-class* track% object% (externalizable<%>)
   (super-new)
   ;public methods
-  (public add-segment get-segment get-length)
+  (public add-segment get-segment get-length get-ids)
   ;variable initialization
   (init)
   (define segments (mutable-set))
+  ;abstraction
+  (define (id seg)(car seg))
+  (define (nodes seg)(cdr seg))
   ;public methods
-  (define (add-segment id node1 node2)
-    (define segment (cons id (cons node1 node2)))
+  (define (add-segment id_ node1 node2)
+    (define segment (cons id_ (cons node1 node2)))
     (set-add! segments segment)
-    (send node1 add-segment id)
-    (send node2 add-segment id))
-  (define (get-segment id)
-    (find-segment segments id))
-  (define (get-length id)
-    (let ([seg (find-segment segments id)])
+    (send node1 add-segment id_)
+    (send node2 add-segment id_))
+  (define (get-segment id_)
+    (find-segment (set-copy segments) id_))
+  (define (get-length id_)
+    (let ([seg (find-segment (set-copy segments) id_)])
       (if seg
-        (calculate-length (cdr seg))
+        (calculate-length (nodes seg))
         #f)))
+  (define (get-ids) (list-ids))
   ;private methods
-  (define (find-segment st id)
+  (define (find-segment st id_)
     (if (not (set-empty? st))
       (let ([current (set-first st)])
-        (if (eq? (car current) id)
+        (if (eq? (id current) id_)
           current
           (begin
             (set-remove! st current) 
-            (find-segment st id))))
+            (find-segment st id_))))
       #f))
   (define (calculate-length nodes)
     (let* ([node1 (car nodes)]
@@ -40,6 +44,11 @@
            [c1 (send node1 get-coordinates)]
            [c2 (send node2 get-coordinates)])
       (sqrt (+ (expt (- (car c2) (car c1)) 2) (expt (- (cdr c2) (cdr c1)) 2)))))
+  (define (list-ids)
+    (define ids '())
+    (for*/mutable-set ([seg segments])
+      (set! ids (append ids (list (id seg)))))
+    ids)
   ;externalizable interface
   (define/public (externalize) segments)
   (define/public (internalize s) (set! segments s)))
