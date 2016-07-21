839ccd6a637756e0d4d64ccc8d6060a412923ed3
kobellem <kobellem@vub.ac.be>
2016-07-21 16:25:36 +0200

    segment has its own class now

---
+++ src/infra/data/segment.rkt
@@ -0,0 +1,38 @@
+#lang racket
+;Author Koen Bellemans
+
+(provide make-segment)
+
+(define (make-segment id node1 node2)
+  (let ([segment (new segment%)])
+    (send* segment (set-id! id)(set-nodes! node1 node2))
+    segment))
+
+(define-serializable-class* segment% object% (externalizable<%>)
+  (super-new)
+  (public set-id! get-id set-nodes! get-nodes get-length)
+  ;no initialization arguments to support serialization
+  (define id #f)
+  (define nodes #f)
+  (define l #f)
+  ;public methods
+  (define (set-id! id_)(set! id id_))
+  (define (get-id) id)
+  (define (set-nodes! node1 node2)
+    (set! nodes (cons node1 node2))
+    (calculate-length))
+  (define (get-nodes) nodes)
+  (define (get-length) l)
+  ;private methods
+  (define (calculate-length)
+    (let* ([node1 (car nodes)]
+           [node2 (cdr nodes)]
+           [c1 (send node1 get-coordinates)]
+           [c2 (send node2 get-coordinates)])
+      (set!  l (sqrt (+ (expt (- (car c2) (car c1)) 2) (expt (- (cdr c2) (cdr c1)) 2))))))
+  ;externalizable interface
+  (define/public (externalize)(list id nodes))
+  (define/public (internalize lst)
+    (set-id! (car lst))
+    (println (cdr lst))
+    (set-nodes! (caadr lst)(cdadr lst))))
--- src/infra/data/track.rkt
+++ src/infra/data/track.rkt
@@ -2,31 +2,27 @@
 ;Author Koen Bellemans
 
 (require racket/set)
+(require "segment.rkt")
 (provide track%)
 
 (define-serializable-class* track% object% (externalizable<%>)
   (super-new)
   ;public methods
-  (public add-segment get-segment get-length get-ids)
+  (public add-segment get-segment get-ids)
   ;variable initialization
   (init)
   (define segments (mutable-set))
   ;abstraction
-  (define (id seg)(car seg))
-  (define (nodes seg)(cdr seg))
+  (define (id seg)(send seg get-id))
+  (define (nodes seg)(send seg get-nodes))
   ;public methods
   (define (add-segment id_ node1 node2)
-    (define segment (cons id_ (cons node1 node2)))
-    (set-add! segments segment)
-    (send node1 add-segment id_)
-    (send node2 add-segment id_))
+    (let ([segment (make-segment id_ node1 node2)])
+      (set-add! segments segment)
+      (send node1 add-segment id_)
+      (send node2 add-segment id_)))
   (define (get-segment id_)
     (find-segment (set-copy segments) id_))
-  (define (get-length id_)
-    (let ([seg (find-segment (set-copy segments) id_)])
-      (if seg
-        (calculate-length (nodes seg))
-        #f)))
   (define (get-ids) (list-ids))
   ;private methods
   (define (find-segment st id_)
@@ -38,12 +34,6 @@
             (set-remove! st current) 
             (find-segment st id_))))
       #f))
-  (define (calculate-length nodes)
-    (let* ([node1 (car nodes)]
-           [node2 (cdr nodes)]
-           [c1 (send node1 get-coordinates)]
-           [c2 (send node2 get-coordinates)])
-      (sqrt (+ (expt (- (car c2) (car c1)) 2) (expt (- (cdr c2) (cdr c1)) 2)))))
   (define (list-ids)
     (define ids '())
     (for*/mutable-set ([seg segments])
