9365acd061318a240013b4bcd539073282679762
kobellem <kobellem@vub.ac.be>
2016-07-22 15:37:36 +0200

    requests with arguments now possible

--- src/nmbs/tcpRequester.rkt
+++ src/nmbs/tcpRequester.rkt
@@ -12,12 +12,13 @@
     (init-field host)
     (init-field port)
     ;public methods
-    (define (request msg) (send msg))
-    (define (request-serialized msg) (deserialize (send msg)))
+    (define (request msg [args #f])(send msg args))
+    (define (request-serialized msg [args #f])(deserialize (send msg args)))
     ;private methods
-    (define (send msg)
-      (define-values (in out) (tcp-connect host port))
+    (define (send msg args)
+      (define-values (in out)(tcp-connect host port))
       (write msg out)
+      (when args (write args out))
       (flush-output out)
       (let ([answer (read in)])
         (close-input-port in)
