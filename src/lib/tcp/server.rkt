30d200defaca2e97599680b9f74aff4e68e1b7a8
kobellem <kobellem@vub.ac.be>
2016-07-31 17:23:32 +0200

    added sleep to allow other threads to execute

--- src/lib/tcp/server.rkt
+++ src/lib/tcp/server.rkt
@@ -13,10 +13,13 @@
     (define listener (tcp-listen port))
     ;public methods
     (define (listen reader)
+    (sleep 5)
     (println (string-append "Server now listening on port " (number->string port)))
       (let loop ()
         (define-values (in out) (tcp-accept listener))
         (reader in out)
         (close-input-port in)
         (close-output-port out)
+        ;give control to other threads
+        (sleep 0)
         (loop)))))
