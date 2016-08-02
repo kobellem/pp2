01e29c0462b47b75601d0d0595cb6cba21504b29
kobellem <kobellem@vub.ac.be>
2016-08-02 18:24:21 +0200

    improved switch mechanics

--- src/lib/data/node.rkt
+++ src/lib/data/node.rkt
@@ -12,14 +12,15 @@
 (define-serializable-class* node% object% (externalizable<%>)
   (super-new)
   ;public methods
-  (public set-id! get-id set-coordinates! get-x get-y add-segment get-segments is-switch? switch)
+  (public set-id! get-id set-coordinates! get-x get-y add-segment get-segments get-alt-segments is-switch? switch is-switched?)
   ;variable initialization
   ;No initialization arguments to support serialization
   (define id #f)
   (define coordinates #f)
   (define seg1 #f)
   (define seg2 #f)
-  (define seg3 #f)
+  (define alt-seg #f)
+  (define switched? #f)
   ;public methods
   (define (set-id! id_) (set! id id_))
   (define (get-id) id)
@@ -30,21 +31,28 @@
     (cond
       [(not seg1) (set! seg1 seg)]
       [(not seg2) (set! seg2 seg)]
-      [(not seg3) (set! seg3 seg)]
+      [(not alt-seg) (set! alt-seg seg)]
       [else (error "Node" id "already has 3 segments")]))
   (define (get-segments) (cons seg1 seg2))
-  (define (is-switch?) (if seg3 #t #f))
-  (define (switch) (if (is-switch?) (swap-seg) #f))
+  (define (get-alt-segments)(if (is-switch?)(cons seg1 alt-seg) #f))
+  (define (is-switch?) (if alt-seg #t #f))
+  (define (switch) (if (is-switch?)
+                     (if switched?
+                       (set! switched? #f)
+                       (set! switched? #t))
+                     #f)
+                   #t)
+  (define (is-switched?) switched?)
   ;private methods
   (define (swap-seg)
     (let ([temp seg2])
-      (set! seg2 seg3)
-      (set! seg3 temp)))
+      (set! seg2 alt-seg)
+      (set! alt-seg temp)))
   ;externalizable interface
-  (define/public (externalize) (list id coordinates seg1 seg2 seg3))
+  (define/public (externalize) (list id coordinates seg1 seg2 alt-seg))
   (define/public (internalize lst)
     (set-id! (car lst))
     (set-coordinates! (caadr lst) (cdadr lst))
     (set! seg1 (caddr lst))
     (set! seg2 (cadddr lst))
-    (set! seg3 (car (cddddr lst)))))
+    (set! alt-seg (car (cddddr lst)))))
