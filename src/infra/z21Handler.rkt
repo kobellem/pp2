0b08edbf99a2f318afa27f7f60820b3bde0c3b95
kobellem <kobellem@vub.ac.be>
2016-08-05 08:31:53 +0200

    theoretical z21Handler

---
+++ src/infra/z21Handler.rkt
@@ -0,0 +1,82 @@
+#lang racket
+;Author Koen Bellemans
+
+(require (prefix-in z21: "z21-API/Z21Socket.rkt"))
+(require (prefix-in z21: "z21-API/Z21MessageSysStat.rkt"))
+(require (prefix-in z21:"z21-API/Z21MessageSettings.rkt"))
+(require (prefix-in z21:"z21-API/Z21MessageDriving.rkt"))
+(require (prefix-in z21:"z21-API/Z21MessageLocation.rkt"))
+(require (prefix-in z21:"z21-API/Z21MessageSwitches.rkt"))
+(require (prefix-in z21:"z21-API/Z21MessageDecoderCV.rkt"))
+(require (prefix-in z21: "z21-API/racket-bits-utils-master/bits.rkt"))
+(provide z21Handler%)
+
+(define z21Handler%
+  (class object%
+    (super-new)
+    (public init request)
+    ;variable initialization
+    (define socket #f)
+    (define listenThread #f)
+    ;public methods
+    (define (init)
+      (set! socket (z21:setup))
+      (set! listenThread (make-listenThread)))
+    (define (request req [args #f])
+      (cond
+        [(string=? req "get-positions")(get-positions)]
+        [(string=? req "switch-state")(switch-state args)]
+        [(string=? req "set-speed")(set-speed args)]
+        [(string=? req "switch")(switch args)]
+        [else "Unknown request"]))
+    ;private methods
+    (define (get-positions)
+      (z21:send socket (z21:make-rmbus-get-data-msg "00"))
+      (let* ([answer (thread-send listenThread "get-answer")]
+             [data (z21:get-rmbus-data answer)]
+             [positions '()])
+        (let outer-loop ([module (z21:get-module (car data))])
+          (let inner-loop ([occupancies (z21:get-occupancies (car data))])
+            (when (not (empty? occupancies))
+              (begin
+                (set! positions (append positions (list (+ (* module 10)(car occupancies)))))
+                (inner-loop (cdr occupancies)))))
+          (set! data (cdr data))
+          (when (not (empty? data))(outer-loop (z21:get-module (car data)))))
+        positions))
+    (define (set-speed args)
+      (let* ([id (car args)]
+             [speed (cadr args)]
+             [dir (caddr args)]
+             [msg (z21:make-set-loco-drive-msg id "00" 128 dir speed)])
+        (send socket msg)
+        (println (thread-send listenThread "get-answer"))))
+    (define (switch-state args)
+      (let* ([id (car args)]
+             [msg (z21:make-get-switch-info-msg (number->string (- id 1)) "00")])
+        (send socket msg))
+      (let ([answer (thread-send listenThread "get-answer")])
+        (if (eq? (z21:get-switch-info-position answer) 1) #t #f)))
+    (define (switch args)
+      (let* ([id (car args)]
+             [state (cadr args)]
+             [state-n (if state 1 2)]
+             [msg (z21:make-set-switch-msg (number->string (- id 1)) "00" true state-n)])
+        (send socket msg)
+        (println (thread-send listenThread "get-answer"))))
+    ;the listen thread for receiving answers from Z21
+    (define (make-listenThread)
+      (thread (lambda ()
+        (define answer #f)
+        (z21:listen socket (lambda (a)
+          (set! answer a)))
+        (let loop ([msg (thread-receive)])
+          (cond
+            [(string=? msg "get-answer")
+              (let inner-loop ()                ;check if the answer has been received already
+                (if answer answer (sleep 0.1))  ;if not, sleep for 0.1 and check again
+                (inner-loop))]
+          [else "Unknwonw message"])
+        (set! answer #f)
+        (loop (thread-receive))))))
+))
